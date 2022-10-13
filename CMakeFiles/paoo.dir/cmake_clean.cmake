b3c79d5bf9236e25058c143a145f5be4bd026fd3
Student <student@b418b-17.intranet.cs.upt.ro>
2022-10-13 16:46:37 +0300

    setup CMake

---
+++ CMakeFiles/paoo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/paoo.dir/lab3.cpp.o"
+  "paoo"
+  "paoo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/paoo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
